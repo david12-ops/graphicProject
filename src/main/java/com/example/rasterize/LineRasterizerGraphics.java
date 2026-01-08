3f9b37ad9bcaae40bbfcda3f1521ffc0e6e4fd06
david12-ops <77929483+david12-ops@users.noreply.github.com>
2026-01-08 15:07:36 +0100

    commnet update

--- src/main/java/com/example/rasterize/LineRasterizerGraphics.java
+++ src/main/java/com/example/rasterize/LineRasterizerGraphics.java
@@ -13,7 +13,6 @@ public class LineRasterizerGraphics extends LineRasterizer {
     /**
      * Creates a rasterizer algorithm instance.
      * 
-     * 
      * @param raster    Raster where the line drawing algorithm will be performed
      * @param colorMode Decides which color will be used for drawing line
      */
