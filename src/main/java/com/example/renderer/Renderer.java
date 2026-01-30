27ef346d2c8902eb92273562eae15f86773c575c
david12-ops <77929483+david12-ops@users.noreply.github.com>
2026-01-30 23:48:45 +0100

    rerender update (bugs elemination)

--- src/main/java/com/example/renderer/Renderer.java
+++ src/main/java/com/example/renderer/Renderer.java
@@ -9,13 +9,13 @@ import com.example.model.Point;
 
 public class Renderer {
     private LineRasterizer lineRasterizer;
-    private int width, heigth;
+    private int width, height;
     private Mat4 view, proj;
 
-    public Renderer(LineRasterizer lineRasterizer, int width, int heigth, Mat4 view, Mat4 proj) {
+    public Renderer(LineRasterizer lineRasterizer, int width, int height, Mat4 view, Mat4 proj) {
         this.lineRasterizer = lineRasterizer;
         this.width = width;
-        this.heigth = heigth;
+        this.height = height;
         this.view = view;
         this.proj = proj;
     }
@@ -29,29 +29,22 @@ public class Renderer {
             Point3D pointB = solid.getVb().get(indexB);
 
             // Modelovací transformace (model) = model space -> world space
-            pointA = pointA.mul(solid.getModel());
-            pointB = pointB.mul(solid.getModel());
-
-            // pointA = pointA.mul(solid.getModel()).mul(view).mul(proj);
-
             // Pohledová tranformace (view) = world space -> view space
-            pointA = pointA.mul(view);
-            pointB = pointB.mul(view);
-
             // Projekční tranformace (projection) = view space -> clip space
-            pointA = pointA.mul(proj);
-            pointB = pointB.mul(proj);
+            pointA = pointA.mul(solid.getModel()).mul(view).mul(proj);
+            pointB = pointB.mul(solid.getModel()).mul(view).mul(proj);
 
-            // TODO: Ořezání - slide 88
-            if (outsideEdge(pointA) && outsideEdge(pointB))
-                continue;
-
+            // reject points behind camera
             if (pointA.getW() <= 0 || pointB.getW() <= 0)
                 continue;
 
-            // TODO: Dehomogenizace - x, y, z, w = x/w, y/w, z/w, w/w = NDC
+            // Dehomogenizace
             pointA = pointA.mul(1.0 / pointA.getW());
             pointB = pointB.mul(1.0 / pointB.getW());
+
+            // Ořezání v clip space
+            if (!insideClipVolume(pointA) || !insideClipVolume(pointB))
+                continue;
 
             // Transformace do okna obrazovky = NDC -> screen space
             Vec3D vecA = transformToWindow(pointA);
@@ -65,23 +58,16 @@ public class Renderer {
         }
     }
 
-    private boolean outsideEdge(Point3D p) {
-        // všechna x jsou větší než -w a
-        // všechna x jsou menší než w a
-        // všechna y jsou větší než -w a
-        // šechna y jsou menší než w a
-        // šechna z jsou větší než 0 a
-        // všechna z jsou menší než w => splněné všechny podmínky => bod je mimo hranici
-
-        return p.getX() > -p.getW() && p.getX() < p.getW() &&
-                p.getY() > -p.getW() && p.getY() < p.getW() && p.getZ() > 0
-                && p.getZ() < p.getW();
+    private boolean insideClipVolume(Point3D p) {
+        return p.getX() >= -p.getW() && p.getX() <= p.getW() &&
+                p.getY() >= -p.getW() && p.getY() <= p.getW() && p.getZ() >= 0
+                && p.getZ() <= p.getW();
     }
 
     private Vec3D transformToWindow(Point3D p) {
         return new Vec3D(p).mul(new Vec3D(1, -1, 1))
                 .add(new Vec3D(1, 1, 0))
-                .mul(new Vec3D((width - 1) / 2., (heigth - 1) / 2., 1));
+                .mul(new Vec3D((width - 1) / 2., (height - 1) / 2., 1));
     }
 
     public void setView(Mat4 view) {
