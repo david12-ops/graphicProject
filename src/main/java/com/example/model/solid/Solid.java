7a05e9d7d598068f6fe8683c5218c9ca657fd466
david12-ops <77929483+david12-ops@users.noreply.github.com>
2026-01-31 10:20:22 +0100

    added state for color switching

--- src/main/java/com/example/model/solid/Solid.java
+++ src/main/java/com/example/model/solid/Solid.java
@@ -1,5 +1,6 @@
 package com.example.model.solid;
 
+import com.example.enums.SolidState;
 import com.example.transforms.Col;
 import com.example.transforms.Mat4;
 import com.example.transforms.Mat4Identity;
@@ -13,6 +14,7 @@ public abstract class Solid {
     protected List<Integer> ib = new ArrayList<>();
     protected Col color = new Col(0xffffff);
     protected Mat4 model = new Mat4Identity();
+    protected SolidState state = SolidState.NORMAL;
 
     public List<Point3D> getVb() {
         return vb;
@@ -28,6 +30,14 @@ public abstract class Solid {
 
     public Mat4 getModel() {
         return model;
+    }
+
+    public SolidState getState() {
+        return state;
+    }
+
+    public void setState(SolidState state) {
+        this.state = state;
     }
 
     public void setModel(Mat4 model) {
