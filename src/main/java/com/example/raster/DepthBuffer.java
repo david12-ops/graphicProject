0f2d473f5dc4ff2f4575dd645550fe854e8cd6a6
david12-ops <77929483+david12-ops@users.noreply.github.com>
2026-05-06 14:43:16 +0200

    update (bounds check for getValue and setValue)

--- src/main/java/com/example/raster/DepthBuffer.java
+++ src/main/java/com/example/raster/DepthBuffer.java
@@ -13,6 +13,25 @@ public class DepthBuffer implements Raster<Double> {
         zBuffer = new double[width][height];
     }
 
+    /**
+     * Checks whether the given pixel coordinates lie inside the raster bounds.
+     *
+     * <p>
+     * A coordinate is considered inside if:
+     * <ul>
+     * <li>{@code x} is in the range {@code [0, image.getWidth())}</li>
+     * <li>{@code y} is in the range {@code [0, image.getHeight())}</li>
+     * </ul>
+     *
+     * @param x the x-coordinate of the pixel
+     * @param y the y-coordinate of the pixel
+     * @return {@code true} if the coordinates are inside the raster,
+     *         {@code false} otherwise
+     */
+    private boolean isInsideRaster(int x, int y) {
+        return x >= 0 && y >= 0 && x < width && y < height;
+    }
+
     @Override
     public void clear() {
         for (int x = 0; x < getWidth(); x++) {
@@ -34,11 +53,15 @@ public class DepthBuffer implements Raster<Double> {
 
     @Override
     public Optional<Double> getValue(int x, int y) {
+        if (!isInsideRaster(x, y))
+            return Optional.empty();
+
         return Optional.of(zBuffer[x][y]);
     }
 
     @Override
     public void setValue(int x, int y, Double value) {
-        zBuffer[x][y] = value;
+        if (isInsideRaster(x, y))
+            zBuffer[x][y] = value;
     }
 }
