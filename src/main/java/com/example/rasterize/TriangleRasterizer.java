a77041deb7517602b651e4d65f81b7f2c2365c5e
david12-ops <77929483+david12-ops@users.noreply.github.com>
2026-05-07 17:50:16 +0200

    triangleRasterizer update

--- src/main/java/com/example/rasterize/TriangleRasterizer.java
+++ src/main/java/com/example/rasterize/TriangleRasterizer.java
@@ -14,23 +14,67 @@ public class TriangleRasterizer {
 
     public void rasterize(Vertex a, Vertex b, Vertex c, Shader shader) {
         // TODO: seřadit vrcholy podle y od min po max
+        // ab
+        if (a.getY() > b.getY()) {
+            Vertex temp = a;
+            a = b;
+            b = temp;
+        }
+        // bc
+        if (b.getY() > c.getY()) {
+            Vertex temp = b;
+            b = c;
+            c = temp;
+        }
+        // ab
+        if (a.getY() > b.getY()) {
+            Vertex temp = a;
+            a = b;
+            b = temp;
+        }
+
+        System.out.println("Rasterizing triangle with vertices: " + a + ", " + b + ", " + c);
 
         Lerp<Vertex> lerp = new Lerp<>();
 
         // 1. část
-        for (int y = (int) a.getY(); y <= (int) b.getY(); y++) {
+        for (int y = (int) a.getY(); y < (int) b.getY(); y++) {
             // Hrana AB
-            double tAB = (y - a.getY()) / (b.getY() - a.getY());
+            double dyAB = b.getY() - a.getY();
+            double tAB = 0;
+            if (dyAB != 0) {
+                tAB = (y - a.getY()) / (b.getY() - a.getY());
+            }
             Vertex ab = lerp.lerp(a, b, tAB);
 
             // Hrana AC
-            double tAC = (y - a.getY()) / (c.getY() - a.getY());
+            double dyAC = c.getY() - a.getY();
+            double tAC = 0;
+            if (dyAC != 0) {
+                tAC = (y - a.getY()) / (c.getY() - a.getY());
+            }
             Vertex ac = lerp.lerp(a, c, tAC);
 
             // TODO: kontrola, jestli je ab.getX() < ac.getX()
+            if (ab.getX() > ac.getX()) {
+                Vertex temp = ab;
+                ab = ac;
+                ac = temp;
+            }
 
             for (int x = (int) Math.round(ab.getX()); x <= (int) Math.round(ac.getX()); x++) {
-                double t = (x - ab.getX()) / (ac.getX() - ab.getX());
+                double dx = ac.getX() - ab.getX();
+                if (dx == 0) {
+                    Vertex pixel = ab;
+                    zBuffer.setPixelWithZTest(
+                            x,
+                            y,
+                            pixel.getZ(),
+                            shader.getColor(pixel));
+                    continue;
+                }
+
+                double t = (x - ab.getX()) / dx;
                 Vertex pixel = lerp.lerp(ab, ac, t);
 
                 zBuffer.setPixelWithZTest(x, y, pixel.getZ(), shader.getColor(pixel));
@@ -38,6 +82,49 @@ public class TriangleRasterizer {
         }
 
         // 2. část
+        // B -> C
+        // A -> C
+        for (int y = (int) b.getY(); y <= (int) c.getY(); y++) {
+            // Hrana BC
+            double dyBC = c.getY() - b.getY();
+            double tBC = 0;
+            if (dyBC != 0) {
+                tBC = (y - b.getY()) / (c.getY() - b.getY());
+            }
+            Vertex bc = lerp.lerp(b, c, tBC);
 
+            // Hrana AC
+            double dyAC = c.getY() - a.getY();
+            double tAC = 0;
+            if (dyAC != 0) {
+                tAC = (y - a.getY()) / (c.getY() - a.getY());
+            }
+            Vertex ac = lerp.lerp(a, c, tAC);
+
+            // TODO: kontrola, jestli je bc.getX() < ac.getX()
+            if (bc.getX() > ac.getX()) {
+                Vertex temp = bc;
+                bc = ac;
+                ac = temp;
+            }
+
+            for (int x = (int) Math.round(bc.getX()); x <= (int) Math.round(ac.getX()); x++) {
+                double dx = ac.getX() - bc.getX();
+                if (dx == 0) {
+                    Vertex pixel = bc;
+                    zBuffer.setPixelWithZTest(
+                            x,
+                            y,
+                            pixel.getZ(),
+                            shader.getColor(pixel));
+                    continue;
+                }
+
+                double t = (x - bc.getX()) / dx;
+                Vertex pixel = lerp.lerp(bc, ac, t);
+
+                zBuffer.setPixelWithZTest(x, y, pixel.getZ(), shader.getColor(pixel));
+            }
+        }
     }
 }
